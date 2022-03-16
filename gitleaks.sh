d8f34759cd1f03714e48584c19cbf6471162acce
jonopcontino <jono.powell@contino.io>
2022-03-16 20:19:05 +0000

    Added stuff for gitleaks

--- gitleaks.sh
+++ gitleaks.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
 pip install pre-commit
+
+cd ..
+git clone https://github.com/zricethezav/gitleaks.git
+
+cd gitleaks
+make build
+
+cd ../secret-scanner-test
+cp gitleaks/.pre-commit-config.yaml .
+pre-commit install 
+
