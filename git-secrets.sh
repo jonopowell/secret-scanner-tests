21139ee55c5411de82c8f070c9f57b3f3bc24eb5
jonopcontino <jono.powell@contino.io>
2022-03-17 15:01:02 +0000

    update: initial git-secrets test

--- git-secrets.sh
+++ git-secrets.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 
+cd 
+git clone https://github.com/awslabs/git-secrets.git
+
+export PATH=${PATH}:~/git-secrets
+
+git-secrets --install
 
 git config --global user.name "jonopowell"
 git config --global user.email "jono.powell@contino.io"
-git checkout -b gitleaks origin/gitleaks
+git pull
+
 
 echo "  " >> MyApp.py
 echo "  " >> aws-gitleaks.json
